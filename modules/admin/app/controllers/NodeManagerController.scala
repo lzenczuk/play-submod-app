2b66d61bdd6f26fe074dc0a24a81bd5fc787ab80
Lucjan Zenczuk <lucjan.zenczuk@gmail.com>
2016-11-14 16:53:52 +0000

    Basic support for management of node in cluster

--- modules/admin/app/controllers/NodeManagerController.scala
+++ modules/admin/app/controllers/NodeManagerController.scala
@@ -5,8 +5,11 @@ import javax.inject.{Inject, Named, Singleton}
 import akka.actor.ActorRef
 import akka.pattern.ask
 import akka.util.Timeout
+import com.github.lzenczuk.cn.cluster.NodeManagerActor
+import com.github.lzenczuk.cn.cluster.NodeManagerActor._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.mvc.{Action, Controller}
+import play.api.libs.json.{JsError, JsResult, Json}
+import play.api.mvc.{Action, BodyParsers, Controller}
 
 import scala.concurrent.duration._
 
@@ -16,12 +19,42 @@ import scala.concurrent.duration._
 @Singleton
 class NodeManagerController @Inject()(@Named("node-manager-actor") nodeManagerActor: ActorRef) extends Controller{
 
-  implicit val timeout: Timeout = 5.seconds
+  implicit val actorAskTimeout: Timeout = 5.seconds
 
-  def ping = Action.async{
-    (nodeManagerActor ? "Hello").mapTo[String].map{
-      msg => Ok(msg)
+  implicit val clusterMemberWrites = Json.writes[ClusterMember]
+  implicit val nodeStatusWrites = Json.writes[NodeStatus]
+
+  implicit val clusterAddressReads = Json.reads[ClusterAddress]
+
+  def status = Action.async{
+    (nodeManagerActor ? NodeManagerActor.GetNodeStatus).mapTo[NodeStatus].map{
+      ns => Ok(Json.toJson(ns))
     }
+  }
+
+  def createCluster = Action{
+    nodeManagerActor ! CreateCluster
+    Ok(Json.obj("status" -> "OK", "message" -> "Create cluster request send"))
+  }
+
+  def joinCluster = Action(BodyParsers.parse.json){ request =>
+
+    val clusterAddressResult = request.body.validate[ClusterAddress]
+
+    clusterAddressResult.fold(
+      errors => {
+        BadRequest(Json.obj("status" ->"OK", "message" -> JsError.toJson(errors)))
+      },
+      clusterAddress => {
+        nodeManagerActor ! JoinCluster(clusterAddress)
+        Ok(Json.obj("status" -> "OK", "message" -> "Join cluster request send"))
+      }
+    )
+  }
+
+  def leaveCluster = Action{
+    nodeManagerActor ! LeaveCluster
+    Ok(Json.obj("status" -> "OK", "message" -> "Leave cluster request send"))
   }
 
 }
--- modules/cluster/src/main/scala/com/github/lzenczuk/cn/cluster/NodeManagerActor.scala
+++ modules/cluster/src/main/scala/com/github/lzenczuk/cn/cluster/NodeManagerActor.scala
@@ -1,6 +1,8 @@
 package com.github.lzenczuk.cn.cluster
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, Address, Props}
+import akka.cluster.MemberStatus._
+import akka.cluster.{Cluster, MemberStatus, UniqueAddress}
 
 /**
   * Created by dev on 14/11/16.
@@ -8,13 +10,58 @@ import akka.actor.{Actor, ActorLogging, Props}
 object NodeManagerActor {
   def props = Props[NodeManagerActor]
 
+  case class ClusterMember(address:String, roles:String, status:String)
+  case class ClusterAddress(systemName: String, host: String, port: Int)
+
+  // NodeManagerActor messages
+  case object GetNodeStatus
+  case class NodeStatus(address:String, inCluster:Boolean, clusterLeader:Option[String], clusterMembers: Set[ClusterMember])
+  case object CreateCluster
+  case class JoinCluster(clusterAddress: ClusterAddress)
+  case object LeaveCluster
+
+  def uniqueAddressToResponseString(ua:UniqueAddress):String = {
+    s"${ua.address}#${ua.longUid}"
+  }
 }
 
 class NodeManagerActor extends Actor with ActorLogging {
 
+  import NodeManagerActor._
+
+  private val cluster: Cluster = Cluster(context.system)
+
+  implicit def memberStatusToString(memberStatus:MemberStatus):String = {
+    memberStatus match {
+      case Joining => "Joining"
+      case WeaklyUp => "WeaklyUp"
+      case Up => "Up"
+      case Leaving => "Leaving"
+      case Exiting => "Exiting"
+      case Down => "Down"
+      case Removed => "Removed"
+      case _ => "Unknown"
+    }
+  }
+
   def receive = {
-    case s:String =>
-      log.info(s"Receive message: $s")
-      sender ! "NodeManagerResponse: hello sender"
+    case GetNodeStatus =>
+
+      val address: String = uniqueAddressToResponseString(cluster.selfUniqueAddress)
+      val leaderOption: Option[Address] = cluster.state.leader
+      val clusterMembers: Set[ClusterMember] = cluster.state.members.map(member =>
+        ClusterMember(uniqueAddressToResponseString(member.uniqueAddress), member.roles.mkString(","), member.status))
+
+      sender ! NodeStatus(address, leaderOption.isDefined, leaderOption.map(_.toString), clusterMembers)
+
+    case JoinCluster(clusterAddress) =>
+      val ca = Address("akka.tcp", clusterAddress.systemName, clusterAddress.host, clusterAddress.port)
+      cluster.join(ca)
+
+    case CreateCluster =>
+      cluster.join(cluster.selfAddress)
+
+    case LeaveCluster =>
+      cluster.leave(cluster.selfAddress)
   }
 }
